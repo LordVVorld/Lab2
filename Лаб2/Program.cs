efa73d01f48e6c93a0d9b6c34544b8b9d5dfeee7
LordVVorld <79798484+LordVVorld@users.noreply.github.com>
2021-02-28 22:32:12 +0700

    Lab2

---
+++ Лаб2/Program.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Security.AccessControl;
+using System.Security.Principal;
+using System.IO;
+
+namespace Лаб2
+{
+    class Programm
+    {
+        static void Main(string[] args)
+        {
+            Document.Instance.DataСollection();
+            Document.Instance.DataOutput();
+            Console.ReadKey();
+        }
+    }
+
+    public class Document
+    {
+        private static Document instance;
+        public string name, author, keyWords, theme, adress;
+        public Document()
+        { }
+
+        public static Document Instance
+        {
+            get
+            {
+                if (instance == null) instance = new Document();
+                return instance;
+            }
+        }
+
+        public virtual void DataСollection()
+        {
+            Console.WriteLine("Введите тему документа: ");
+            theme = Console.ReadLine();
+            Console.WriteLine("Введите ключевые слова документа: ");
+            keyWords = Console.ReadLine();
+            Console.WriteLine("Введите путь к документу: ");
+            adress = Console.ReadLine();
+            FileInfo fileInfo = new FileInfo(adress);
+            FileSecurity fileSecurity = fileInfo.GetAccessControl();
+            IdentityReference identityReference = fileSecurity.GetOwner(typeof(NTAccount));
+            author = identityReference.Value;
+            name = Path.GetFileName(adress);
+        }
+
+        public virtual void DataOutput()
+        {
+            Console.WriteLine("Имя документа: " + name);
+            Console.WriteLine("Автор документа: " + author);
+            Console.WriteLine("Ключевые слова документа: " + keyWords);
+            Console.WriteLine("Тема документа: " + theme);
+            Console.WriteLine("Путь к документу: " + adress);
+        }
+    }
+
+    public class MSWord: Document
+    {
+        public MSWord()
+        { }
+        public override void DataСollection()
+        {
+            keyWords = Console.ReadLine();
+            theme = Console.ReadLine();
+            adress = Console.ReadLine();
+            FileInfo fileInfo = new FileInfo(adress);
+            FileSecurity fileSecurity = fileInfo.GetAccessControl();
+            IdentityReference identityReference = fileSecurity.GetOwner(typeof(NTAccount));
+            author = identityReference.Value;
+            name = Path.GetFileName(adress);
+        }
+    }
+    public class PDF: Document
+    {
+        private PDF()
+        { }
+    }
+    public class MSExcel: Document
+    {
+        private MSExcel()
+        { }
+    }
+    public class TXT : Document
+    {
+        private TXT()
+        { }
+    }
+    public class HTML : Document
+    {
+        private HTML()
+        { }
+    }
+}
---
+++ Лаб2/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Общие сведения об этой сборке предоставляются следующим набором
+// набора атрибутов. Измените значения этих атрибутов для изменения сведений,
+// связанные с этой сборкой.
+[assembly: AssemblyTitle("Лаб2")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Лаб2")]
+[assembly: AssemblyCopyright("Copyright ©  2021")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Установка значения False для параметра ComVisible делает типы в этой сборке невидимыми
+// для компонентов COM. Если необходимо обратиться к типу в этой сборке через
+// из модели COM задайте для атрибута ComVisible этого типа значение true.
+[assembly: ComVisible(false)]
+
+// Следующий GUID представляет идентификатор typelib, если этот проект доступен из модели COM
+[assembly: Guid("07c1a27e-e158-4b45-8823-b5071236d924")]
+
+// Сведения о версии сборки состоят из указанных ниже четырех значений:
+//
+//      Основной номер версии
+//      Дополнительный номер версии
+//      Номер сборки
+//      Номер редакции
+//
+// Можно задать все значения или принять номера сборки и редакции по умолчанию 
+// используя "*", как показано ниже:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
